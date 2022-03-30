5e0d4a95fcfccd4e8658c0daf9c3f5d3f34548dc
gengminy <kls1998@naver.com>
2022-03-31 00:36:38 +0900

    getting started

---
+++ src/test/java/hello/hellospring/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellospring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
